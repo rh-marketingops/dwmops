f4ae7cacc3f85f5f77e845b9e2a37e23a1f1ff97
colemanja91 <colemanja91@gmail.com>
2016-09-20 15:07:42 -0400

    bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='DWMOps',
-      version='0.0.5',
+      version='0.0.6',
       description='Data Washing Machine implemented on Openshift',
       author='Jeremy Coleman',
       author_email='jecolema@redhat.com'
