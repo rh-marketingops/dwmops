dfb2a245d6a3148fc1ab4ceede190ce138789a15
colemanja91 <colemanja91@gmail.com>
2016-09-22 20:40:53 -0400

    cat to end of output

--- .openshift/cron/hourly/Eloqua_Contacts_DWM.sh
+++ .openshift/cron/hourly/Eloqua_Contacts_DWM.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ "$OPENSHIFT_NAMESPACE" == "marketing" ]; then
-  nohup python $OPENSHIFT_REPO_DIR/Eloqua_Contacts_DWM.py > $OPENSHIFT_LOG_DIR/date +"%Y-%m-%d""-Eloqua_DWM_console.log" 2>&1 &
+  nohup python $OPENSHIFT_REPO_DIR/Eloqua_Contacts_DWM.py >> $OPENSHIFT_LOG_DIR/date +"%Y-%m-%d""-Eloqua_DWM_console.log" 2>&1 &
 else
   echo "not in 'marketing' namespace"
 fi
