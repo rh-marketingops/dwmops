264825676e2fb216df1d6d75bd998b499d1be311
colemanja91 <colemanja91@gmail.com>
2016-11-29 16:02:31 -0500

    fix the script

--- .openshift/cron/minutely/scheduler.sh
+++ .openshift/cron/minutely/scheduler.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 case $(( $(date +%M) % 30 )) in
 0) echo "0 ran" ;; # Run GET job
-[1-28]) "1-28 ran" ;; # Run minutely jobs
-29) "29 ran" ;; # Run POST job
+[1-28]*) echo "1-28 ran" ;; # Run minutely jobs
+29) echo "29 ran" ;; # Run POST job
 esac
