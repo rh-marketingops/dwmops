c61cbc31c773d5971e7d0a918f3717e76f7ed266
colemanja91 <colemanja91@gmail.com>
2016-12-01 11:17:59 -0500

    uncomment queueStats

--- Eloqua_Contacts_CleanQueues.py
+++ Eloqua_Contacts_CleanQueues.py
@@ -44,7 +44,7 @@ for row in queues:
 
     logging.info(row + ' size: ' + str(queueSize))
 
-    #queueStats = cq.getQueueStats()
+    queueStats = cq.getQueueStats()
 
     timeout = cq.timeout(t=300)
 
@@ -55,10 +55,10 @@ for row in queues:
     a.set(queueSize)
     b = Gauge('QueueTimeout_DWM', 'Number of records timed out', registry=registry)
     b.set(timeout)
-    #c = Gauge('QueueMaxCounter', 'Current maximum # of attempts', registry=registry)
-    #c.set(queueStats['_counter']['max'])
-    #d = Gauge('QueueMaxTimestamp', 'Age of oldest in queue', registry=registry)
-    #d.set(queueStats['_timestamp']['max'])
+    c = Gauge('QueueMaxCounter', 'Current maximum # of attempts', registry=registry)
+    c.set(queueStats['_counter']['max'])
+    d = Gauge('QueueMaxTimestamp', 'Age of oldest in queue', registry=registry)
+    d.set(queueStats['_timestamp']['max'])
     push_to_gateway(os.environ['PUSHGATEWAY'], job=jobName + '_' + row, registry=registry)
     del registry
 
