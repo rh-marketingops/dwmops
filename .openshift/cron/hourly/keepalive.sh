b449d8d9eec0a13f3372088b4536601c488341f4
colemanja91 <colemanja91@gmail.com>
2016-12-01 14:04:58 -0500

    add and explanation

--- .openshift/cron/hourly/keepalive.sh
+++ .openshift/cron/hourly/keepalive.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+## This is a keepalive script. Since no web traffic should be going to the Openshift app, it will end up idling
+## When that happens, cron jobs stop and the DB shuts down
+## This script pings the url once an hour to keep it alive
 PATH=/bin:/usr/bin:/usr/sbin
 app_url=http://$OPENSHIFT_APP_DNS/
 
