e0cbe619937139adf9c9e5416ad39bf8a03331b3
colemanja91 <colemanja91@gmail.com>
2016-12-09 07:43:33 -0500

    disable scripts while in dev

--- runscripts/halfhour_getdwm.sh
+++ runscripts/halfhour_getdwm.sh
@@ -4,5 +4,5 @@ if [ "$OPENSHIFT_NAMESPACE" == "marketing" ]; then
   nohup python $OPENSHIFT_REPO_DIR/Eloqua_Contacts_GetDWM.py >> "$OPENSHIFT_LOG_DIR/Eloqua_Contacts_GetDWM_Console_$(date +%Y-%m-%d).log" 2>&1 &
 else
   echo "not PROD"
-  nohup python $OPENSHIFT_REPO_DIR/Eloqua_Contacts_GetDWM.py >> "$OPENSHIFT_LOG_DIR/Eloqua_Contacts_GetDWM_Console_$(date +%Y-%m-%d).log" 2>&1 &
+  #nohup python $OPENSHIFT_REPO_DIR/Eloqua_Contacts_GetDWM.py >> "$OPENSHIFT_LOG_DIR/Eloqua_Contacts_GetDWM_Console_$(date +%Y-%m-%d).log" 2>&1 &
 fi
