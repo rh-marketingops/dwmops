48e16ee2767d67589b78f5def6dfb1a51f47ed90
pramod toraskar <ptoraska@redhat.com>
2017-02-15 17:32:39 +0530

    DWMOps release v0.2.1

--- runscripts/halfhour_postdwm.sh
+++ runscripts/halfhour_postdwm.sh
@@ -4,5 +4,5 @@ if [ "$OPENSHIFT_NAMESPACE" == "marketing" ]; then
   nohup python $OPENSHIFT_REPO_DIR/Eloqua_Contacts_PostDWM.py >> "$OPENSHIFT_LOG_DIR/Eloqua_Contacts_PostDWM_Console_$(date +%Y-%m-%d).log" 2>&1 &
 else
   echo "not PROD"
-  #nohup python $OPENSHIFT_REPO_DIR/Eloqua_Contacts_PostDWM.py >> "$OPENSHIFT_LOG_DIR/Eloqua_Contacts_PostDWM_Console_$(date +%Y-%m-%d).log" 2>&1 &
+  nohup python $OPENSHIFT_REPO_DIR/Eloqua_Contacts_PostDWM.py >> "$OPENSHIFT_LOG_DIR/Eloqua_Contacts_PostDWM_Console_$(date +%Y-%m-%d).log" 2>&1 &
 fi
